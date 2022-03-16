8505ba3cecf47f4d844eef9ee2edc07840791064
Cuan <idgr@tutanota.com>
2022-03-16 16:40:27 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # flac2pod
 Seamlessly converts your existing FLAC library to be played on an iPod Classic.
+
+This script makes use of ffmpeg to convert your existing FLAC music library to either MP3-320 or ALAC for easy iPod Classic use. It also takes advantage of "rg2sc" (https://github.com/rwinkhart/rg2sc) to convert any existing ReplayGain tags to Apple Sound Check tags that are compatible with Apple products.
+
+# Usage
+
+```
+flac2pod -f --mp3  # will convert library to MP3-320 and forcefully update Sound Check tags (even if they already exist)
+
+flac2pod --alac  # will convert library to ALAC in a .m4a container - only updates missing Sound Check tags
+
+flac2pod  # will convert library to MP3-320 - only updates missing Sound Check tags
+```
+
+In order for flac2pod to function, your library must be structured as follows:
+Library dir -> Artist dir -> Album dir -> FLAC files
+
+When prompted for the source directory, you want to input your library parent directory.
+
+# Installation
+
+flac2pod is available in the AUR as "flac2pod". If installing on a non-Arch-based distribution, follow the manual installation instructions:
+
+- copy the flac2pod executable from https://github.com/rwinkhart/flac2pod/blob/master/bin/flac2pod to your /usr/bin/
+- copy the rg2sc executable from https://github.com/rwinkhart/rg2sc/blob/master/bin/rg2sc to your /usr/bin
+- install python-mutagen, ffmpeg, and lame
---
+++ package.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# This script packages flac2pod (from source) for Arch Linux.
+
+echo -e '\n\nThe value entered in this field will only affect the version reported to the package manager. The latest source is used regardless.\n'
+read -r -p "Version number: " version
+read -r -p "Revision number: " revision
+
+echo -e '\nOptions (please enter the number only):'
+echo -e '\n1. GitHub Release Tag\n2. Local\n'
+read -r -p "Source (for build scripts): " source
+
+if [ "$source" == "1" ]; then
+    source='https://github.com/rwinkhart/flac2pod/releases/download/v$pkgver/flac2pod-$pkgver.tar.xz'
+else
+    source=local://flac2pod-"$version".tar.xz
+fi
+
+
+# Archive creation
+echo -e '\nPackaging as generic...\n'
+mkdir -p packages/archtemp/usr
+cp -r bin packages/archtemp/usr/
+cp -r share packages/archtemp/usr/
+tar -C packages/archtemp -cvf packages/flac2pod-"$version".tar.xz usr/
+rm -rf packages/archtemp
+sha512="$(sha512sum packages/flac2pod-"$version".tar.xz | awk '{print $1;}')"
+echo -e "\nsha512 sum:\n$sha512"
+echo -e "\nGeneric packaging complete.\n"
+
+# PKGBUILD creation
+echo -e '\nGenerating PKGBUILD...'
+echo "# Maintainer: Randall Winkhart <idgr at tutanota dot com>
+
+pkgname=flac2pod
+pkgver="$version"
+pkgrel="$revision"
+pkgdesc='Converts your FLAC library to be iPod-ready'
+url='https://github.com/rwinkhart/flac2pod'
+arch=('any')
+license=('GPL2')
+depends=(python ffmpeg lame rg2sc)
+
+source=(\""$source"\")
+sha512sums=('"$sha512"')
+
+package() {
+
+    tar xf flac2pod-"\"\$pkgver\"".tar.xz -C "\"\${pkgdir}\""
+
+}
+" > packages/PKGBUILD
+    echo -e "\nPKGBUILD generated.\n"
